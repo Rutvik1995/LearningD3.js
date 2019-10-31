76a59eac3f0cbd8ada44757372ec227262b8f6f4
Rutvik <rutvikpatel.rm@gmail.com>
2019-10-30 19:53:41 -0700

    Minnor Change

---
+++ data.js
@@ -0,0 +1,11 @@
+var dataset =[1,2,3,4,5];
+
+d3.select("body")
+    .selectAll('p')
+    .data(dataset)
+    .enter()
+    .append('P')
+    //.text('D3 is awesome')
+    .text(function(d){
+        return d;
+    });
---
+++ intro.js
@@ -0,0 +1,14 @@
+alert("hello");
+
+
+d3.select("h1").style("color", "blue")
+.text("Update the text");
+
+
+d3.select('body').append('p').text('First Para ');
+d3.select('body').append('p').text('Second Para ');
+d3.select('body').append('p').text('Third Para ');
+d3.select('body').append('p').text('Fourth Para ');
+
+
+d3.selectAll('p').style('color','red');
